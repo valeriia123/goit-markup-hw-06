7dd3819b048fd2d698bdff9ec683503ba8c8cfaf
Valeriia Pylypenko <pylypenko.valeriia1@gmail.com>
2024-10-08 19:32:28 +0100

    add mobile, tablet and desktop styles

--- js/modal.js
+++ js/modal.js
@@ -14,21 +14,3 @@
     refs.modal.classList.toggle("is-open");
   }
 })();
-
-
-(() => {
-  const refs = {
-    // Додати атрибут data-modal-open на кнопку відкриття
-    openNavigationBtn: document.querySelector("[data-navigation-open]"),
-    // Додати атрибут data-modal-close на кнопку закриття
-    closeNavigationBtn: document.querySelector("[data-navigation-close]"),
-    // Додати атрибут data-modal на бекдроп модалки
-    navigation: document.querySelector("[data-navigation]"),
-  };
-  refs.openNavigationBtn.addEventListener("click", toggleNavigation);
-  refs.closeNavigationBtn.addEventListener("click", toggleNavigation);
-  function toggleNavigation() {
-    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
-    refs.navigation.classList.toggle("is-open");
-  }
-})();
